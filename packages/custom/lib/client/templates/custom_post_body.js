d437b52a0b83f6e3d32deed86144f0752426832c
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-08-22 23:13:30 -0700

    slupdate

--- packages/custom/lib/client/templates/custom_post_body.js
+++ packages/custom/lib/client/templates/custom_post_body.js
@@ -10,6 +10,7 @@ Template.post_body.helpers({
 		var	rp=	-1;
 		var	sp=	-1;
 		var	ep=	-1;
+		var	url=	"";
 		
 		while(true)
 		{
@@ -34,7 +35,19 @@ Template.post_body.helpers({
 				}
 			}while(nested> 0 && rp!== -1);
 			ep=	rp;
-			text=	text.replace(text.substring(sp-5, ep+1), "</p><div class='plot'>"+text.substring(sp, ep)+"</div><p>");
+			url=	"https://rawgit.com/johnRedden/OpenGraph/master/index.html?readType=iframe&"+(text.substring(sp, ep).replace(/\$/g, ""));
+			/*
+				Text here does a couple of things:
+					-First it tries to replace the plot(*) and it gets out of the paragraph.
+					-Second it tries to center the MathJax
+					-Third it tries to setup an iframe that is used by opengraphingcalculator, although I don't know if it is better than rendering it by jsxgraph
+					-Lastly it gives you a link to open it up in opengraphingcalculator
+			*/
+			text=	text.replace(text.substring(sp-5, ep+1), "</p><div class='graphText' style='align-text: center'>"+
+				((text.substring(sp, ep).indexOf("$")!== -1) ? text.substring(sp, ep) : "$ "+text.substring(sp, ep)+" $")+
+				"</div>"+
+				"<iframe src='"+url+"' width='100%' height='200px' scrolling='no'></iframe><br/><button style='width: 100%;max-width:100%;' onclick='"+
+				"location.assign(\""+url.replace("readType=iframe", "readType=text")+"\");'>Open in OpenGraphingCalculator.com</button><p>");
 		}
 		
 		this.htmlBody=	text;
