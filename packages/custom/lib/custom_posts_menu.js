fd9ab8a1446a6524ec00b532e6ea92d85b8ca97e
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-08-25 08:55:03 -0700

    Post Menus

---
+++ packages/custom/lib/custom_posts_menu.js
@@ -0,0 +1,16 @@
+
+
+Template.menuItems.replace("viewsMenu", [
+  {
+    route:  "posts_top",
+    label:  "top",
+    description:  "most_popular_posts"
+  },
+  {
+    route: "posts_new",
+    label:  "new",
+    description:  "newest_posts"
+  }
+]);
+
+// End of File
