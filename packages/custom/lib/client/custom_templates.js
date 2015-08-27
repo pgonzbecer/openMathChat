1caf9120a5e6ee9367090447196d883a0b60f5d6
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-08-27 08:53:47 -0700

    Update custom_templates.js

--- packages/custom/lib/client/custom_templates.js
+++ packages/custom/lib/client/custom_templates.js
@@ -1,4 +1,4 @@
 // Override "post_title" template
 Template.custom_post_title.replaces("post_title");
 Template.custom_post_body.replaces("post_body");
-Template.custom_post_comment.replaces("comment_submit");
+Template.custom_comment_submit.replaces("comment_submit");
